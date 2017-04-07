a459695789fbd6a7448f21f537be1c13ffd06c36
s1103137219 <sink0917@gmail.com>
2017-04-07 15:51:59 +0800

    77777777775

---
+++ MvcApplication1/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class OrderController : Controller
+    {
+        //
+        // GET: /Order/
+
+        public ActionResult Index()
+        {
+            eSaleService.OrderService orderService = new eSaleService.OrderService();
+            var data=orderService.GetOrderById("1");
+            ViewBag.data = data.CustId+"."+data.Custname+"."+data.OrderId;
+            return View();
+        }
+
+    }
+}
---
+++ eSaleDao/OrderDao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eSaleDao
+{
+    public class OrderDao
+    {
+        /// <summary>
+        /// 依訂單編號取得訂單資料
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public eSaleModel.Order GetOrderById(string id)
+        {
+            return new eSaleModel.Order()
+            {CustId="GSS",Custname="瑞陽資訊",OrderId="1"};
+        }
+    }
+}
